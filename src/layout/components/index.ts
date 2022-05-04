f0c74aa6f5a94c9ba9748883ddef1e2f72bc7a22
huangzhenxiang <1442140320@qq.com>
2022-05-04 16:38:47 +0800

    first

---
+++ src/layout/components/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: huangzhenxiang
+ * @Date: 2022-04-26 15:30:39
+ * @LastEditors: huangzhenxiang
+ * @LastEditTime: 2022-04-29 10:06:26
+ */
+export { default as AppMain } from "./AppMain.vue"
+export { default as NavigationBar } from "./NavigationBar/index.vue"
+export { default as Settings } from "./Settings/index.vue"
+export { default as Sidebar } from "./Sidebar/index.vue"
+// export { default as TagsView } from "./TagsView/index.vue"
+export { default as RightPanel } from "./RightPanel/index.vue"
